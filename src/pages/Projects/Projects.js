9c4f185d3f432b70fc7d6d4fb8327687b52edba2
JullyLoureiro <juliana.ferreira@soulasalle.com.br>
2020-06-15 17:13:22 -0300

    Projetos

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import './style.css'
 import {Grid} from '@material-ui/core'
+import Card from '../../components/Card/Card'
 
 export default function Projects(){
     return (
         <Grid container className='containerProjects'>
-            <Grid item xs={12} md={12}>
-                <div>teste</div>
+            <Grid item xs={12} md={3}>
+                <Card 
+                    title='Mob Vendas' 
+                    info='Um app desenvolvido em Java para android direcionado para autônomos, vendedores e microempreendedores gerenciarem suas vendas.' 
+                    img='https://lh3.googleusercontent.com/hH_8nrq4j5DrtNM_H05Pm67piUxOSauETjUjZWGEK0WYtt-d-FrFiDT4kY5VmHxtAQ=h250-rw'
+                />
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card
+                    title='WSGE Gerencial'
+                    info='Ferramenta para Android e IOS feito em React Native para gerenciar empresas que já utilizam o sistema WSGE da JD System.'
+                    img='https://lh3.googleusercontent.com/m7BplRHYNEgFtND5b1Tf5K6wvAKO7Abp3wnDHlWqHpI1QzUksiDnJQ5JBPHcPK-3VDM=w2560-h1156-rw'
+                />
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card
+                    title='Dashboard Covid'
+                    info='Dashboard que consome dados abertos sobre o Covid-19 através de gráficos, desenvolvido através do framework React JS.'
+                    img='https://github.com/JullyLoureiro/dashboard-covid-19/raw/master/src/images/printAppCovid.PNG'
+                />
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <Card
+                    title='Decision AHP'
+                    info='Aplicação para Android desenvolvida em Java, para auxiliar gestores na tomada de decisão multicritério, utilizando a metodologia AHP.'
+                    img='https://raw.githubusercontent.com/JullyLoureiro/projeto-final-ahp/master/app/src/main/res/drawable/logo.png'
+                />
             </Grid>
         </Grid>
     )
--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -1,17 +1,56 @@
 import React from 'react'
 import './style.css'
 import {Grid} from '@material-ui/core'
-import { Progress } from 'semantic-ui-react'
+import { withStyles } from '@material-ui/core/styles'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import ImgSkills from '../../assets/skills.png'
+
+const BorderLinearProgress = withStyles((theme) => ({
+    root: {
+      height: 10,
+      borderRadius: 5,
+    },
+    colorPrimary: {
+      backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+    },
+    bar: {
+      borderRadius: 5,
+      backgroundColor: '#FF0090',
+    },
+  }))(LinearProgress);
 
 export default function Skills(){
+    const Progress = (props) => (
+        <Grid className='progress' item xs={12} md={12}>
+                <BorderLinearProgress variant="determinate" value={props.value} />
+                <label>{props.title}</label>
+        </Grid>
+    )
     return (
-        <Grid container className='containerSkills'>
-            <Grid item xs={12} md={12}>
-                <div>
-                    <p>tetse</p>
-                    <Progress percent={29} color='pink' />
-                </div>
+        <Grid container direction={'row'}>
+            <Grid item xs={12} md={5} className='info'>
+                <Grid container direction={'column'} >
+                    <div >
+                        <span className='textInfo'>Este progress baseia-se em uma auto crítica da minha atual experiência e aprendizado nas tecnologias listadas. Para uma melhor avaliação consulte meus repositórios github e/ou sinta-se à vontade para aplicar testes condizentes.</span>
+                        <br/>
+                        <img className='imgSkills' src={ImgSkills}/>
+                    </div>
+                </Grid>
+            </Grid>
+            <Grid item xs={12} md={7}>
+                <Grid container className='containerSkills'>
+                    <Progress title='React JS' value={75} />
+                    <Progress title='HTML 5' value={65} />
+                    <Progress title='CSS 3' value={60} />
+                    <Progress title='Javascript' value={65} />
+                    <Progress title='Java Android' value={85} />
+                    <Progress title='Node JS' value={35} />
+                    <Progress title='React Native' value={75} />
+                    <Progress title='C#' value={40} /> 
+                    <Progress title='Kotlin' value={30} />
+                </Grid>
             </Grid>
         </Grid>
+       
     )
 }
